839fe7376c05fe904470e47679bf657d96fc59e2
Simple-codinger <vincentgierisch@gmail.com>
2020-11-03 19:57:23 +0100

    add input parser

---
+++ include/util/InputParser.hpp
@@ -0,0 +1,16 @@
+#ifndef INPUTPARSER_HPP
+#define INPUTPARSER_HPP
+
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class InputParser {
+    private:
+        std::vector<std::string> _arguments;
+    public:
+        InputParser(int &argc, char **argv);
+        const std::string& getCmdOption(const std::string &option) const;
+        bool cmdOptionExists(const std::string &option) const;
+};
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,36 @@
+#include <iostream>
 #include "../include/GameHandler.hpp"
+#include "../include/util/InputParser.hpp"
 
-int main() {
-    GameHandler *gameHandler = new GameHandler();
+static void printHelp(){
+	std::cout << "-i --ip\t IP-Adress (default 127.0.0.1)" << std::endl;
+	std::cout << "-p --port\t Port (default 7777)" << std::endl;
+	std::cout << "-h --help\t print help" << std::endl;
+}
+
+int main(int argc, char **argv) {
+
+    // Default parameter
+    std::string ip = "127.0.0.1";
+    unsigned short port = 7777;
+
+    InputParser input(argc, argv);
+    if(input.cmdOptionExists("-h") || input.cmdOptionExists("--help")) {
+        printHelp();
+        return 0;
+    }
+
+    if(input.cmdOptionExists("-i") || input.cmdOptionExists("--ip")) {
+        std::string option = input.getCmdOption("-i");
+        ip = option;
+    }
+
+    if(input.cmdOptionExists("-p") || input.cmdOptionExists("--port")) {
+        std::string option = input.getCmdOption("-p");
+        port = std::stoi(option, nullptr);;
+    }
+
+    GameHandler *gameHandler = new GameHandler(ip, port);
     gameHandler->run();
     return 0;
 }
