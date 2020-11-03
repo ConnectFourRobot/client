40684c75f764cc4c2e86ad71ad968af776f14875
Simple-codinger <vincentgierisch@gmail.com>
2020-11-03 13:43:34 +0100

    remove cgrid and change wording

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -19,24 +19,23 @@ Game::Game(int sizeX, int sizeY, int numberOfPlayers) {
 }
 
 void Game::setStone(Player player, int column, Grid& grid){
-    int y = 0;
+    int row = 0;
     for(int i = 0; i < this->SizeY; i++){
-        if(grid.getStone(x, y) == 0){
-            y++;
+        if(grid.getStone(column, row) == 0){
+            row++;
         }else{
             break;
         }
     }
-    grid.getStone(x, y-1) = player.Id;
+    grid.getStone(column, row - 1) = player.Id;
 }
 
 std::vector<PossibleMove> Game::getPossibleMoves(Player player, Grid grid){
     std::vector<PossibleMove> moves;
     for(int x = 0; x < this->SizeX; x++){
         if(grid.getStone(x, 0) == 0){
-            Grid cgrid(grid);
-            this->setStone(player, x, cgrid);
-            moves.push_back(PossibleMove(x, cgrid));
+            this->setStone(player, x, grid);
+            moves.push_back(PossibleMove(x, grid));
         }
     }
     return moves;
