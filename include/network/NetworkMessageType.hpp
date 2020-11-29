71410ede619f45e0dc20e1fa462f10b6f46f1b05
otf32159 <florian.ott@st.oth-regensburg.de>
2020-11-29 12:33:38 +0100

    feature network: message types

--- include/network/NetworkMessageType.hpp
+++ include/network/NetworkMessageType.hpp
@@ -2,11 +2,11 @@
 #define NETWORKMESSAGETYPE_H
 typedef enum NetworkMessageType
 {
-    Configuration = 1,
-    Request = 2,
-    Answer = 3,
-    Move = 4,
-    EndGame = 5
+    //Configuration = 1,
+    Request = 1,
+    Answer = 2,
+    Move = 3,
+    EndGame = 4
 }
 NetworkMessageType;
 #endif
